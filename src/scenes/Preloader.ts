a3b4c0b01e956dd6ff368ff3e08290be5ef87970
Maciej Hajduk <maciej.hajduk@deviniti.com>
2020-03-21 20:20:10 +0100

    fix/loading_bar

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -2,10 +2,20 @@ import { TILE_SIZE } from "../consts";
 
 export class Preloader extends Phaser.Scene {
     constructor() {
-        super({});
+        super({
+            key: 'preloader',
+            pack: {
+                files: [
+                    { type: 'image', key: 'bar', url: './assets/images/loadBar.png' },
+                    { type: 'image', key: 'barBg', url: './assets/images/barBg.png' }
+                ]
+            }
+        });
     }
 
-    preload() {
+    public preload(): void {
+        this._loadingBar();
+
         this.load.image('tiles', 'assets/tilemaps/tiles/cute_tiles_32.png');
         this.load.image('beach_tiles', 'assets/tilemaps/tiles/beach_tileset.png');
         this.load.tilemapTiledJSON('map', 'assets/tilemaps/json/smalltest.json');
@@ -28,7 +38,24 @@ export class Preloader extends Phaser.Scene {
         this.load.audio('background_music', 'assets/audio/background_music.mp3');
     }
 
-    create() {
+    public create(): void {
         this.scene.start('main');
+    }
+
+    private _loadingBar(): void {
+        const bar = this.add.sprite(this.sys.canvas.width / 2, this.sys.canvas.height / 2, 'bar');
+        const mask = this.make.graphics({
+            x: bar.x - (bar.width / 2),
+            y: bar.y - (bar.height / 2),
+            add: false
+        });
+        mask.fillRect(0, 0, 0, bar.height);
+
+        bar.mask = new Phaser.Display.Masks.GeometryMask(this, mask);
+
+        this.load.on('progress', (progress: number) => {
+            mask.clear();
+            mask.fillRect(0, 0, bar.width * progress, bar.height);
+        });
     }
 }
