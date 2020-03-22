bb6a25aa0b3770bde1adfa3a3ec05689229388af
Paweł Narolski <pawel.narolski@gmail.com>
2020-03-22 21:32:08 +0100

    Added game map files from Tiled.

---
+++ game_map/beach.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="beach_tileset" tilewidth="32" tileheight="32" tilecount="936" columns="36">
+ <image source="../Downloads/tf_beach_tileset/beach_tileset.png" width="1152" height="832"/>
+</tileset>
---
+++ game_map/tilemap2x.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.3" name="tilemap2x" tilewidth="32" tileheight="32" tilecount="600" columns="30">
+ <image source="GuttyKreumCityAssetPack1/Tilemap/tilemap2x.png" width="960" height="640"/>
+</tileset>
